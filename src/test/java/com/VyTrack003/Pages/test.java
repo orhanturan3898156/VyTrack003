fc3f9bd9c774b63f585bd9260fd699c0492d54f2
orhanturan3898156 <orhanturan1566@gmail.com>
2020-07-24 18:11:45 -0400

    commit

---
+++ src/test/java/com/VyTrack003/Pages/test.java
@@ -0,0 +1,7 @@
+package com.VyTrack003.Pages;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hi Guys");
+    }
+}
---
+++ src/test/java/com/VyTrack003/StepDefinitions/test.java
@@ -0,0 +1,7 @@
+package com.VyTrack003.StepDefinitions;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hey Guys");
+    }
+}
