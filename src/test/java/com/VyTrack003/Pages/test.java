7a1f2e43683b9469e1cb52ffcc6c259ea01358a8
orhanturan3898156 <orhanturan1566@gmail.com>
2020-07-25 12:13:37 -0400

    commit

--- src/test/java/com/VyTrack003/Pages/test.java
+++ src/test/java/com/VyTrack003/Pages/test.java
@@ -3,5 +3,6 @@ package com.VyTrack003.Pages;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hi Guys");
+        System.out.println("Hello");
     }
 }
